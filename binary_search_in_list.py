001da24c013178896ba7d98937ad0edabbb70441
unknown <anuroopmishra21@gmail.com>
2020-04-30 20:20:48 +0530

    binary search

--- binary_search_in_list.py
+++ binary_search_in_list.py
@@ -1,1 +1,19 @@
 # searching an element in the list using binary search
+l=list(map(int, input("Enter the list ").split(" ")))
+n=int(input("Enter the element to be searched "))
+c=0
+low=0
+high=len(l)-1
+while low<=high:
+    mid=(low+high)//2
+    if n== l[mid]:
+        c=1
+        break
+    elif n<l[mid]:
+        high=mid-1
+    else:
+        low=mid+1
+if c==1:
+    print("Element found")
+else:
+    print("Element not found")
