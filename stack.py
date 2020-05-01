da4ea4321c54c80004ade543d009d0bcc0b368aa
unknown <anuroopmishra21@gmail.com>
2020-05-01 21:21:10 +0530

    stack

--- stack.py
+++ stack.py
@@ -1,1 +1,12 @@
 #code for stack
+l=[]
+while True:
+    n=int(input("Enter 1 to push, 2 to pop , anything else to abort "))
+    if n==1:
+        l.append(int(input("Enter the element to be pushed ")))
+        print("The updated list is ", l)
+    elif n==2:
+        print(l.pop())
+        print("The updated stack is ", l)
+    else:
+        break
