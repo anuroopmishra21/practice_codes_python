c42baaa39ceb0aba88aae924fee92503b39c5d3e
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-08-08 22:04:36 +0530

    Update list_of_integers.py

--- list_of_integers.py
+++ list_of_integers.py
@@ -1,18 +1,4 @@
-"""Write a function get_products_of_all_ints_except_at_index() that takes a list of integers and returns a list of the products.
-
-For example, given:
-
-  [1, 7, 3, 4]
-
-Python 2.7
-your function would return:
-
-  [84, 12, 28, 21]
-
-Python 2.7
-by calculating:
-
-  [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3] """
+ * 3 * 4,  1 * 7 * 4,  1 * 7 * 3] """
 
 # CODE
 arr =   [1, 7, 3, 4]
