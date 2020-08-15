ffb47efd1bf5ca0cf9253d84abfd7a063775ff2f
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-08-15 23:07:12 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,7 +7,6 @@ def bubbleSort(arr):
         for j in range(0, n-i-1):  
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-# Driver code to test above 
 arr = [64, 34, 25, 12, 22, 11, 90]
 bubbleSort(arr) 
 print ("Sorted array is:") 
