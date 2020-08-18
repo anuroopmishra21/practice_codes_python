54739f04eaac29f930e76d3cd2a8b9bd4e0ba7d6
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-08-18 22:31:53 +0530

    Update fibonacci_with_recursion.py

--- fibonacci_with_recursion.py
+++ fibonacci_with_recursion.py
@@ -1,12 +1,8 @@
 def fibonacci_recursive(n):
     print("Calculating F", "(", n, ")", sep="", end=", ")
-
-    # Base case
     if n == 0:
         return 0
     elif n == 1:
         return 1
-
-    # Recursive case
     else:
         return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
