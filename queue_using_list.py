2fae90b72e4f247fec8de75b12a7af8c743995a5
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-08-24 22:48:07 +0530

    Create queue_using_list.py

---
+++ queue_using_list.py
@@ -0,0 +1,12 @@
+# Python code to demonstrate Implementing  
+# Queue using list 
+queue = ["Amar", "Akbar", "Anthony"] 
+queue.append("Ram") 
+queue.append("Iqbal") 
+print(queue) 
+# Removes the first item 
+print(queue.pop(0)) 
+print(queue)   
+# Removes the first item
+print(queue.pop(0)) 
+print(queue)
