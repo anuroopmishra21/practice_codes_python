4eb0f1342d2229a45f55bcaba675c792d5791c47
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-08-27 22:34:02 +0530

    Create addition_of_matrix.py

---
+++ addition_of_matrix.py
@@ -0,0 +1,14 @@
+M1 = [[1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1]]
+M2 = [[1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]]
+sum = [[0, 0, 0],
+       [0, 0, 0],
+       [0, 0, 0]]
+for i in range(len(M1)):
+    for j in range(len(M1[0])):
+        sum[i][j] = M1[i][j] + M2[i][j]
+for num in sum:
+    print(num)
