ca4c597f4d70975a4726a901e641c6610fcee6be
anuroopmishra21 <41761194+anuroopmishra21@users.noreply.github.com>
2020-09-03 22:49:32 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,11 +1,5 @@
 num = 7
-
-# To take input from the user
-#num = int(input("Enter a number: "))
-
 factorial = 1
-
-# check if the number is negative, positive or zero
 if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
 elif num == 0:
